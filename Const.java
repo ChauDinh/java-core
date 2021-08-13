dad3967a63fd7d73513239f59a4cee663cdd690b
Chau <chaudvb95uc@gmail.com>
2021-08-13 22:32:07 +0700

    java core: const

---
+++ Const.java
@@ -0,0 +1,13 @@
+public class Const {
+  public static void main(String[] args) {
+    final int YEAR_OF_BIRTH;
+    YEAR_OF_BIRTH = 2000;
+    System.out.println("Năm sinh của bạn là: " + YEAR_OF_BIRTH);
+
+    final float PI = 3.14f;
+    System.out.println("Pi: " + PI);
+
+    final String VE_XE_RE = "Vé Xe Rẻ";
+    System.out.println(VE_XE_RE);
+  }
+}
