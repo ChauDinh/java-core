2f3f9a497726b627403aaa4ee80ffbe9ad6d8239
Chau <chaudvb95uc@gmail.com>
2021-08-14 23:43:40 +0700

    java core: operation

---
+++ Operation.java
@@ -0,0 +1,25 @@
+public class Operation {
+  public static void main(String[] args) {
+    int a = 5, b = 2, c = 1;
+    // double da = a, db = b;
+    // double result = 0;
+
+    // // phep chia
+    // result = da / db;
+
+    // System.out.println(a + "/" + b + " = " + result);
+
+    // int result = a % b;
+    // System.out.println(a + "%" + b + " = " + result);
+
+    // boolean result = a >= b;
+    // System.out.println("result: " + result);
+
+    // boolean result = a == b;
+    // System.out.println("result: " + result);
+
+    Integer number = 1;
+    boolean result = number instanceof Integer;
+    System.out.println("result: " + result);
+  }
+}
