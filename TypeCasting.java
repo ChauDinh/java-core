29e70ad04529b10cf9c6d90366d89b5bc05c433b
Chau <chaudvb95uc@gmail.com>
2021-08-22 00:03:01 +0700

    java core: string

--- TypeCasting.java
+++ TypeCasting.java
@@ -26,5 +26,50 @@ public class TypeCasting {
     System.out.println("shortTotal: " + shortTotal);
     System.out.println("byteTotal: " + byteTotal);
     System.out.println("=====================");
+
+    // Strings, substrings, concatenation
+
+    String greeting = "Hello";
+    String s = greeting.substring(0, 3);
+    System.out.println(s);
+    // the length of substring s is b - a
+
+    String all = String.join("/", "S", "M", "L", "XL");
+    System.out.println(all);
+
+    String repeated = "Java".repeat(3);
+    System.out.println(repeated);
+
+    // Strings are immutable
+    greeting = greeting.substring(0, 3) + "p!";
+    System.out.println(greeting);
+
+    System.out.println("Hello".equals(greeting));
+    System.out.println("Help!".equals(greeting));
+    System.out.println("help!".equalsIgnoreCase(greeting));
+    System.out.println("Help!" == greeting);
+
+    System.out.println(greeting.length() == 0);
+    System.out.println(greeting.equals(""));
+    System.out.println(greeting == null);
+
+    if (greeting != null && !greeting.equals("")) {
+      System.out.println(greeting);
+    }
+
+    if (greeting != null && greeting.length() != 0) {
+      System.out.println(greeting);
+    }
+
+    int cpCount = greeting.codePointCount(0, greeting.length());
+    System.out.println(cpCount);
+
+    char first = greeting.charAt(0);
+    char last = greeting.charAt(greeting.length() - 1);
+    System.out.println(first + " " + last);
+
+    int index = greeting.offsetByCodePoints(0, 2);
+    int cp = greeting.codePointAt(index);
+    System.out.println(cp);
   }
 }
